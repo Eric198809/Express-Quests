808f7502d175975995546fdc62289fb2d405e418
ERIC BESSON <e-besson@laposte.net>
2024-02-12 14:48:45 +0100

    ok

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ const app = express();
 
 const movieControllers = require("./controllers/movieControllers");
 
+/////Movies///////
+
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
+/////Users///////
+
+app.get("/api/users", movieControllers.getUsers);
+app.get("/api/users/:id", movieControllers.getUserById);
 
 module.exports = app;
