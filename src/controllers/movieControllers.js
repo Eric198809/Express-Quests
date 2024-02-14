ed52d2bc03371cce689c82019e2916426840ab78
ERIC BESSON <e-besson@laposte.net>
2024-02-14 10:31:52 +0100

    quete PUT ok

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -51,9 +51,33 @@ const postMovie = (req, res) => {
     });
 };
 
+/////Update movie///////
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 
 module.exports = {
   getMovies,
   getMovieById,
   postMovie,
+  updateMovie,
 };
